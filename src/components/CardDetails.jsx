97130d61aa0a2504ec9e2aaf59e90f39f8d3b007
luckyluke <llcodes@outlook.com>
2024-04-06 08:54:44 -0400

    Fixed text wrap on large screens

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -3,7 +3,7 @@ import CardButton from "./CardButton";
 const CardDetails = ({ name, status, btnColor, followers, textColor }) => {
   return (
     <div className="flex h-full w-full flex-col items-end justify-center overflow-hidden">
-      <div className="flex w-full flex-col items-center justify-center gap-2 py-4">
+      <div className="flex w-full shrink-0 flex-col items-center justify-center gap-2 truncate py-4">
         <span className={`text-sm font-semibold ${textColor}`}>{status}</span>
         <span className="text-xl font-semibold">{name}</span>
         <span className="text-sm text-slate-500">{followers} Followers</span>
